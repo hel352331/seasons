45691e8cc548be1b6b29ef660af348bfebeaf44d
hel352331 <alenka352331@gmail.com>
2017-10-31 22:59:51 +0200

    ready

--- second.js
+++ second.js
@@ -1,43 +1,44 @@
-var january = '1';
-var february = '2';
-var march = '3';
-var april ='4';
-var may = '5';
-var june = '6';
-var july = '7';
-var august = '8';
-var september ='9';
-var october = '10';
-var november = '11';
-var december = '12';
-var month = 0;
-while(month == 0){
-var input_month = prompt("Input the month of your birth", "1").toLowerCase();
-if (input_month != " "){
-  month = 1;
-}
-}
+var is_over = false;
+while(is_over == false){
+  var input_month = prompt('Input the month of your birth', '1').toLowerCase();
   switch (input_month) {
-    case january:
-    case february:
-    case december:
+    case 'january':
+    case 'february':
+    case 'december':
+    case '1':
+    case '2':
+    case '12':
+      is_over == true;
       alert('Hello in winter!');
       break;
-    case march:
-    case april:
-    case may:
+    case 'march':
+    case 'april':
+    case 'may':
+    case '3':
+    case '4':
+    case '5':
+      is_over == true;
       alert('Hello in spring!');
       break;
-    case june:
-    case july:
-    case august:
+    case 'june':
+    case 'july':
+    case 'august':
+    case '6':
+    case '7':
+    case '8':
+      is_over == true;
       alert('Hello in summer!');
       break;
-    case september:
-    case october:
-    case november:
+    case 'september':
+    case 'october':
+    case 'november':
+    case '9':
+    case '10':
+    case '11':
+      is_over == true;
       alert('Hello in autumn!');
       break;
     default:
       alert('This month does not exist!')
   }
+}
